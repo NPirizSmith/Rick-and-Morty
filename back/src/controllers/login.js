7827ae0c04e72c3f300ff9e6b9e0950584cc5fed
NPirizSmith <nahuelpirizsmith2@gmail.com>
2024-01-31 18:35:06 -0500

    user login

--- back/src/controllers/login.js
+++ back/src/controllers/login.js
@@ -4,18 +4,19 @@ const login = async (req, res) => {
   try {
     const { email, password } = req.query;
     if (!email || !password) return res.status(400).send("Faltan datos");
-    
-    const userFind = await User.findOne({ where: { email }
-    });
-    
+
+    const userFind = await User.findOne({ where: { email } });
+
     if (!userFind) return res.status(404).json({ error: "Usuario no encontrado" });
-    
-    return userFind.password === password ? res.json({access: true}) :
-    res.status(403).send("Contraseña incorrecta")
-    
-  }catch (error) {
-    return res.status(500).send(error.message)
+
+    if (userFind.password === password) {
+      return res.json({ access: true });
+    } else {
+      return res.status(403).send("Contraseña incorrecta");
+    }
+  } catch (error) {
+    return res.status(500).send(error.message);
   }
 };
 
-module.exports = login
+module.exports = login;
