8df2e96afba0dd43d413aea1f0730cbfea2885be
NPirizSmith <nahuelpirizsmith2@gmail.com>
2024-01-31 21:46:04 -0500

    fix

--- back/src/controllers/postUser.js
+++ back/src/controllers/postUser.js
@@ -1,24 +1,22 @@
-// Importa el modelo User
-const { User } = require("../DB_connection");
+const { User } = require("../DB_connection")
 
-const postUser = async (req, res) => {
-    try {
+const postUser = async (req, res)=>{
+  try {
+const { email, password } = req.body
+if (!email || !password) {
+  return res.status(400).send("Faltan datos");
+}
+const user = await User.findOrCreate({
+  where: {
+    email, 
+    password
+  }
+});
+return res.status(200).json(user);
+   
+} catch (error) {
+    return res.status(500).json( { error: error.message} );
+}
+}
 
-        const { email, password } = req.body;
-
-
-        if (!email || !password) {
-            return res.status(400).send("Faltan datos");
-        }
-
-        const [user, created] = await User.findOrCreate({
-            where: { email, password }
-        });
-
-        return res.status(200).json(user);
-    } catch (error) {
-        return res.status(500).json({ error: error.message });
-    }
-};
-
-module.exports = postUser;
+module.exports= postUser
